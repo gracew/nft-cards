dcc17c974c9f56322338091bcf09ee3fef2ab89c
Grace Wang <gracewang92@gmail.com>
2022-12-23 16:07:01 +0100

    more cleanup

--- components/snapsRecipient.tsx
+++ components/snapsRecipient.tsx
@@ -1,47 +1,13 @@
 import { ethers } from "ethers";
-import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import { AuthType, MAINNET_PROVIDER } from '../auth';
-import ButtonContainer from './buttonContainer';
-import Nav from './nav';
-import Or from './or';
-import PrimaryButton from './primaryButton';
-import SecondaryButton from './secondaryButton';
 
 interface SnapsRecipientProps {
   existingData?: any;
 }
 
-// https://stackoverflow.com/a/46181
-function validateEmail(email: string) {
-  return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-};
-
 const SnapsRecipient = ({ existingData }: SnapsRecipientProps) => {
-  const router = useRouter();
-  const [recipientType, setRecipientType] = useState<AuthType | undefined>(existingData?.recipient_type as AuthType);
-  const [recipientName, setRecipientName] = useState<string>(existingData?.recipient_fname || "");
-  const [recipientEmail, setRecipientEmail] = useState<string>(existingData?.recipient_email || "");
   const [recipientAddress, setRecipientAddress] = useState<string>(existingData?.recipient_wallet_address || "");
-  const [validEmail, setValidEmail] = useState<boolean | undefined>();
   const [validAddress, setValidAddress] = useState<boolean | undefined>();
-  const [resolvedAddress, setResolvedAddress] = useState<string>();
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const delayDebounceFn = setTimeout(() => {
-      // only validate email after user has stopped typing for 1s
-      if (recipientEmail) {
-        setValidEmail(validateEmail(recipientEmail) !== null);
-      }
-    }, 1000)
-
-    return () => clearTimeout(delayDebounceFn)
-  }, [recipientEmail])
 
   useEffect(() => {
     const delayDebounceFn = setTimeout(async () => {
@@ -65,107 +31,14 @@ const SnapsRecipient = ({ existingData }: SnapsRecipientProps) => {
     return () => clearTimeout(delayDebounceFn)
   }, [recipientAddress])
 
-  function disabled() {
-    if (recipientType === AuthType.EMAIL) {
-      return !recipientName || !validEmail;
-    } else if (recipientType === AuthType.ADDRESS) {
-      return !validAddress;
-    }
-    return false;
-  }
-
-  async function createSnaps() {
-    const data = recipientType === AuthType.EMAIL
-      ? { recipientName, recipientEmail }
-      : { recipientAddress: resolvedAddress || recipientAddress };
-    const snaps = await fetch('/api/createSnaps', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        recipientType,
-        ...data,
-      }),
-    }).then(res => res.json());
-    return snaps.id;
-  }
-
-  async function updateSnaps() {
-    const data = recipientType === AuthType.EMAIL
-      ? { recipientName, recipientEmail }
-      : { recipientAddress: resolvedAddress || recipientAddress };
-    await fetch('/api/updateSnaps', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        id: existingData!.id,
-        recipientType,
-        ...data,
-      }),
-    }).then(res => res.json());
-    return existingData!.id;
-  }
-
-  async function onNext() {
-    setLoading(true);
-    const newId = await (existingData ? updateSnaps() : createSnaps());
-    router.push(`/give/${newId}/category`);
-  }
-
-
   return (
     <div className="w-80 flex flex-col">
-      <Nav hideGiveSnaps={true} />
       <h1 className="text-2xl font-bold mt-5 mb-3">
         Give Snaps and Spread ❤️
       </h1>
       <h2 className="my-2">Send a shoutout to a friend in 3 simple steps. They&apos;ll be able to claim it for free.</h2>
       <h2 className="my-2">Who are you sending to?</h2>
 
-      {!recipientType && <div className="flex flex-col">
-        <PrimaryButton
-          text="Email"
-          onClick={() => setRecipientType(AuthType.EMAIL)}
-        />
-        <Or />
-        <PrimaryButton
-          text="Ethereum Address"
-          onClick={() => setRecipientType(AuthType.ADDRESS)}
-        />
-      </div>}
-
-      {recipientType === "email" && <div>
-        <label className="block text-sm font-medium text-gray-300 mt-3">First Name</label>
-        <div className="mt-1 relative rounded-md shadow-sm">
-          <input
-            type="text"
-            name="fname"
-            id="fname"
-            className="bg-gray-800 focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-500 rounded-md "
-            value={recipientName}
-            onChange={(e) => setRecipientName(e.target.value)}
-          />
-        </div>
-        <p className="mt-2 text-xs text-gray-400">This will be publicly visible.</p>
-
-        <label className="block text-sm font-medium text-gray-300 mt-3">Email</label>
-        <div className="mt-1 relative rounded-md shadow-sm">
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className={`bg-gray-800 focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm rounded-md ${validEmail === undefined || validEmail ? "border-gray-500" : "border-pink-500 text-pink-600"}`}
-            value={recipientEmail}
-            onChange={(e) => setRecipientEmail(e.target.value)}
-          />
-        </div>
-        <p className="mt-2 text-xs text-gray-400">This is private and will only be used to notify your friend.</p>
-      </div>}
-
-      {recipientType === "address" &&
         <div>
           <label className="block text-sm font-medium text-gray-300 mt-3">Ethereum Address or ENS</label>
           <div className="mt-1 relative rounded-md shadow-sm">
@@ -179,20 +52,7 @@ const SnapsRecipient = ({ existingData }: SnapsRecipientProps) => {
             />
           </div>
 
-        </div>}
-      {recipientType && <ButtonContainer>
-        <SecondaryButton
-          text="Back"
-          onClick={() => setRecipientType(undefined)}
-        />
-        <PrimaryButton
-          text="Next"
-          onClick={onNext}
-          disabled={disabled()}
-          loading={loading}
-        />
-      </ButtonContainer>}
-
+        </div>
     </div>
   )
 }
--- pages/claim/[id].tsx
+++ pages/claim/[id].tsx
@@ -1,16 +1,11 @@
 import type { GetServerSideProps, NextPage } from 'next';
 import Head from 'next/head';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useContext, useEffect, useState } from 'react';
-import { AuthType, connect, walletLogin } from '../../auth';
-import { resolveCategory } from '../../category';
 import Card from '../../components/card';
-import GoogleButton from '../../components/googleButton';
 import LargeSpinner from '../../components/largeSpinner';
 import MintPanel from '../../components/mintPanel';
 import MintPanelContents from '../../components/mintPanelContents';
-import Nav from '../../components/nav';
 import PrimaryButton from '../../components/primaryButton';
 import Share from '../../components/share';
 import { shortenAddress } from '../../components/shortenedAddress';
@@ -25,19 +20,6 @@ const SnapsDetails: NextPage = (props: any) => {
   const [me, setMe] = useContext(UserContext);
   const [minting, setMinting] = useState(false);
   const [matchingEmail, setMatchingEmail] = useState(false);
-
-  async function getMe() {
-    return fetch('/api/me', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({}),
-    })
-      .then(res => res.json())
-      .then(setMe);
-  };
-
 
   useEffect(() => {
     if (id) {
@@ -70,77 +52,14 @@ const SnapsDetails: NextPage = (props: any) => {
       });
   }
 
-  async function onClickConnect() {
-    const res = await connect();
-    await walletLogin(res);
-    getMe();
-    // TODO: handle failure case
-  }
-
   function getInnerComponent() {
-    const connectWallet = (
-      <MintPanelContents
-        text="If you own the address that this collectible was sent to, connect your wallet to claim."
-      >
-        <PrimaryButton text="Connect Wallet" onClick={onClickConnect} />
-      </MintPanelContents>
-    );
-    const connectEmail = (
-      <MintPanelContents
-        text="If you own the email address that this collectible was sent to, log in with Google to claim."
-      >
-        <GoogleButton />
-      </MintPanelContents>
-    );
-    const mint = (
+    return (
       <MintPanelContents
         text="This won't cost you any transaction fees."
       >
         <PrimaryButton text="Claim as NFT" onClick={mintNFT} loading={minting} />
       </MintPanelContents>
     );
-
-    if (snaps!.recipient_type === AuthType.ADDRESS) {
-      if (!me || !me.address) {
-        return connectWallet;
-      } else {
-        if (me.address.toLowerCase() === snaps!.recipient_wallet_address?.toLowerCase()) {
-          return mint;
-        }
-        // else user address doesn't match the recipient address
-      }
-    } else {
-      // recipient_type is email
-      if (!me || !me.email) {
-        return connectEmail;
-      } else {
-        if (matchingEmail) {
-          if (me.address) {
-            return mint;
-          } else {
-            return (
-              <div className="flex flex-col pb-3">
-                <div className="mb-4">
-                  Congrats, this collectible is now available in <span className="text-lime-200 hover:text-lime-300">
-                    <Link href="/snaps">your dashboard</Link></span>! If you want
-                  to turn this collectible into an NFT, you&apos;ll need to connect a crypto wallet. If you don&apos;t
-                  have one yet, check out <a
-                    href="https://medium.com/@helena.gagern/how-to-mint-your-first-nft-for-free-6a252bb59824"
-                    className="text-lime-200 hover:text-lime-300"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    our guide
-                  </a> for setting one up.
-                </div>
-                <PrimaryButton text="Connect Wallet" onClick={onClickConnect} />
-              </div>
-            )
-          }
-        }
-        // else user email doesn't match the recipient email
-      }
-    }
   }
 
   function getOpenSeaUrl() {
@@ -160,19 +79,6 @@ const SnapsDetails: NextPage = (props: any) => {
     return `Snaps: ${sender} sent ${recipient} a collectible shoutout`;
   }
 
-  function getVideoTag() {
-    if (props.category?.video_url) {
-      return (
-        <>
-          <meta property="og:video" content={props.category.video_url} />
-          <meta property="og:video:type" content="video" />
-          <meta property="og:video:width" content="720" />
-          <meta property="og:video:height" content="1280" />
-        </>
-      )
-    }
-  }
-
   const inner = getInnerComponent();
   const isSender = me && me.sub.toLowerCase() === snaps.sender_id.toLowerCase();
   const claimable = !snaps.minted_at && !isSender && inner !== undefined;
@@ -184,21 +90,12 @@ const SnapsDetails: NextPage = (props: any) => {
         <title>{getTitle()}</title>
         <meta name="description" content="Send shoutouts to teammates and colleagues as digital collectibles." />
         <meta key="image" property="og:image" content={props.category.image_url} />
-        {getVideoTag()}
 
         <meta name="twitter:title" content={getTitle()} />
         <meta name="twitter:description" content="Send shoutouts to teammates and colleagues as digital collectibles." />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:image" content={props.category.image_url} />
       </Head>
-
-      <Nav />
-      {isSender && snaps.recipient_type === AuthType.ADDRESS && <div className='bg-gray-800 rounded-lg my-5 px-5 py-3'>
-        We&apos;ve sent this shoutout to your friend&apos;s wallet address 🤩 Let them know by sharing the link below.
-      </div>}
-      {isSender && snaps.recipient_type === AuthType.EMAIL && <div className='bg-gray-800 rounded-lg my-5 px-5 py-3'>
-        We&apos;ll email your friend and let them know of your appreciation 🙂
-      </div>}
 
       {!snaps && <>
         <div className="flex flex-col min-h-screen items-center justify-center">
@@ -284,8 +181,6 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   if (snaps.recipient_wallet_address) {
     props.recipient = (await shortenAddress(snaps.recipient_wallet_address));
   }
-
-  props.category = await resolveCategory(snaps.category);
 
   const isSafari = /^((?!chrome|android).)*safari/i.test(context.req.headers['user-agent']!);
   props.isSafari = isSafari;
